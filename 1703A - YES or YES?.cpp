409d6e5657aec7f683ebb511ec57fdc952aaa421
Afjol Hussain <95563785+Afjol-77@users.noreply.github.com>
2022-12-26 22:55:30 +0600

    Create 1703A - YES or YES?.cpp

---
+++ 1703A - YES or YES?.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    
+    while(t--){
+        string s;
+        cin >> s;
+        
+        for(int i=0;i<s.size();i++){
+            s[i] = tolower(s[i]);
+        }
+        
+        if(s[0]== 'y' && s[1]== 'e' && s[2]== 's')
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO" <<endl;
+    }
+    
+    return 0;
+}
