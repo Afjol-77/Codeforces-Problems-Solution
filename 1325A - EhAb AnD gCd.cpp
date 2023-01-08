4966e1c0685f7393f54d3236f336569c6481457b
Afjol Hussain <95563785+Afjol-77@users.noreply.github.com>
2023-01-08 19:11:50 +0600

    Create 1325A - EhAb AnD gCd.cpp

---
+++ 1325A - EhAb AnD gCd.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int x; cin >> x;
+        int a,b;
+        if(x%2==0){
+            a=x/2;
+            b=x/2;
+        }
+        
+        else{
+            a=x-1;
+            b=1;
+        }
+        cout<<a<<" "<<b<<endl;
+        }
+    return 0;
+}
