2c64f9d7ee20b3c25180c311f57e9fec22c5d827
Afjol Hussain <95563785+Afjol-77@users.noreply.github.com>
2023-01-16 00:08:32 +0600

    Create 1726A - Mainak and Array.cpp

---
+++ 1726A - Mainak and Array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    int a[n];
+        
+    for(int i=0;i<n;i++)
+        cin >>a[i];
+        
+    int ans=a[n-1]-a[0];
+
+    for(int i=1;i<=n-1;i++)
+        ans=max(ans,a[i]-a[0]);
+    for(int i=0;i<=n-2;i++)
+        ans=max(ans,a[n-1]-a[i]);
+    for(int i=1;i<=n-1;i++)
+        ans=max(ans,a[i-1]-a[i]);
+        
+    cout<<ans<<endl;
+}
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
