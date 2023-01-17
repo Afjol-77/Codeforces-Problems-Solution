adf69531b93634fb473114882d5e3c4bf169397c
Afjol Hussain <95563785+Afjol-77@users.noreply.github.com>
2023-01-17 18:37:02 +0600

    Create 1186A - Vus the Cossack and a Contest.cpp

---
+++ 1186A - Vus the Cossack and a Contest.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+   int n,m,k; 
+   cin >> n>>m>>k;
+   
+   if(m>=n && k>=n)
+       cout<<"YES";
+    else
+        cout<<"NO";
+}
+
+int main(){
+    solve();
+    return 0;
+}
