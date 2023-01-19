11d0c0406c1413049b1fccc5849a8c75b0a37b39
Afjol Hussain <95563785+Afjol-77@users.noreply.github.com>
2023-01-20 00:46:36 +0600

    Create 1372A - Omkar and Completion.cpp

---
+++ 1372A - Omkar and Completion.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long long int n;
+    cin>>n;
+    
+    for(int i=0;i<n;i++)
+        cout<<1<<" ";
+    
+    cout<<endl;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
